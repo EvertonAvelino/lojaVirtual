d4454b23b5d5049c4c2b73040266916616bca913
EvertonAvelino <everton-vitor@hotmail.com>
2021-04-06 23:20:21 -0300

    add imagem dos produtos

--- index.php
+++ index.php
@@ -13,9 +13,23 @@ include_once './conexao.php';
     <?php
         echo "<h2>Produtos</h2>";
 
-        $query_products = "SELECT id, name FROM products ORDER BY id DESC";
+        $query_products = "SELECT id, name, price, image FROM products ORDER BY id ASC";
         $result_products = $conn->prepare($query_products);
         $result_products->execute();
+
+        while($row_product = $result_products->fetch(PDO::FETCH_ASSOC)){
+           
+            extract($row_product);
+
+            echo "<img src='./images/$id/$image'><br>";
+            echo "ID: $id <br>";
+            echo "Nome: $name <br>";
+            echo "Preço: R$".number_format($price,2,",",".")."<br>";
+            echo "<hr>";
+        }
+
+        
+
 
     ?>
 </body>
